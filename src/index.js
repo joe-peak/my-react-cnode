a995fd320de392c406e5449679e15917670dd24f
Zhou Changlong <zhouchanglong@yatang.net>
2017-12-26 14:04:40 +0800

    recover

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ import { Provider } from 'react-redux';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { store } from '@redux/store';
-const RootApp = () => {
-    return (
-        <Provider store={store}>
-            <App/>
-        </Provider>
-    );
-}
+// const RootApp = () => {
+//     return (
+//         <Provider store={store}>
+//             <App/>
+//         </Provider>
+//     );
+// }
 
 const render = Component => {
     const App =  () => (
